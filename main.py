e00082310c52dd6852b550aab4eb6e72e3903e84
theCoderSchool Montclair <alexanderjcantrell@gmail.com>
2026-05-21 17:19:57 -0400

    re added main.py

---
+++ main.py
@@ -0,0 +1,66 @@
+"""
+TODO:
+Collision
+Tile animations
+"""
+#System functions & 2D Graphics functionality
+import sys, pygame
+#Player and tile manager classes
+from entities.player import Player
+from map.tileManager import TileManager
+#For reading settings.yaml
+import yaml
+
+#Runner class for managing all of the game's functions
+class Runner:
+    def __init__(self):
+        #Make constants private instead of local
+        global FPS, BLK
+        #Initialize Pygame
+        pygame.init()
+        #Get global variables from settings.yaml
+        self.settings = yaml.safe_load(open("./settings.yaml"))
+        self.SCALE = self.settings["scale"] #Screen upscale amount
+        self.columns = self.settings["columns"]
+        self.rows = self.settings["rows"]
+        #Create a TileManager object for map loading & drawing
+        self.tm = TileManager(self)
+        #Create screen where all the sprites get drawn to
+        self.width = self.tm.columns * 16 * self.SCALE
+        self.height = self.tm.rows * 16 * self.SCALE
+        self.screen = pygame.display.set_mode((self.width, self.height))
+        pygame.display.set_caption("Game")
+        #Create a player object
+        self.player1 = Player(self)
+        #Constants
+        FPS = 30
+        BLK = (0, 0, 0)
+        
+    def run(self, map: str):
+        """
+        The main function responsible for the game loop and functions, limited by the FPS constant
+        """
+        self.tm.loadMap(map)
+        #Define clock
+        clock = pygame.time.Clock()
+        #Game loop
+        while True:
+            #Limit FPS
+            clock.tick(FPS)
+            #Stop pygame when window closed
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT: sys.exit()
+            #Refresh screen
+            self.screen.fill(BLK)
+            #Draw tiles, entities and player to the screen
+            self.tm.drawMap()
+            self.player1.update()
+            #On-screen FPS counter for benchmarking
+            #self.screen.blit(pygame.font.Font(None, 18).render(f"{clock.get_fps():2.0f} FPS" , True, (255, 255, 255)), (10, 10))
+            #Apply changes to the screen
+            pygame.display.flip()
+
+#Create an instance of the runner class
+game = Runner()
+#Run game loop
+game.run("start")
